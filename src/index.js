df84f784ef70d179a1994599e4824f7aa2ca8752
nawap07 <pawanmahatoop07@gmail.com>
2024-10-20 07:57:08 +0530

    ADD DataBase connction

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// require('dotenv).config();
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+});
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//This is a first approche;
+ /* import express from "express";
+const app=express();
+;(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("Error",()=>{
+            console.log("ERROR");
+            throw error
+        })
+
+        app.listen(process.env.PORT ,()=>{
+            console.log(`App is listen at PORT : - ${process.env.PORT}`);
+            
+        })
+    } catch (error) {
+        console.log("Error",error);
+        
+    }
+})()   */
