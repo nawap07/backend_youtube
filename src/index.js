d6c021e8bd95d45eb1d4a18e3ec7bfcac346f0ca
nawap07 <pawanmahatoop07@gmail.com>
2024-10-20 12:24:44 +0530

    Api Error and Response

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is running at PORT :- ${process.env.PORT}`);
+    })
+
+    app.on("error" ,(error)=>{
+        console.log("ERR" ,error);
+        throw error
+    })
+})
+.catch((error)=>{
+    console.log(" MONGODB connection FAILED : -ERROR : -" , error);
+    
+})
 
 
 
